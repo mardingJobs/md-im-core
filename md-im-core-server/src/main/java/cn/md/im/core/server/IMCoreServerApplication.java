c70e3889385eecebd7ac75515f60c0c6a42db404
martin <14334614+martin-jobs@user.noreply.gitee.com>
2024-09-05 20:00:03 +0800

    init

---
+++ md-im-core-server/src/main/java/cn/md/im/core/server/IMCoreServerApplication.java
@@ -0,0 +1,18 @@
+package cn.md.im.core.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * * @Author: Martin
+ * * @Date    2024/9/5 19:53
+ * * @Description
+ **/
+@SpringBootApplication
+public class IMCoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IMCoreServerApplication.class);
+    }
+
+}
